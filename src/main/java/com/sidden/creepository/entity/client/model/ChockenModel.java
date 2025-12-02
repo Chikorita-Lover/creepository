20a1045b88d343a089d1d02e7752fd5e9403ca33
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2025-12-02 23:34:28 +0200

    Chocken Model Fix

--- src/main/java/com/sidden/creepository/entity/client/model/ChockenModel.java
+++ src/main/java/com/sidden/creepository/entity/client/model/ChockenModel.java
@@ -16,68 +16,64 @@ import net.neoforged.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ChockenModel<T extends Entity> extends AgeableListModel<T> {
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(Creepository.MOD_ID, "chocken"), "main");
-    private final ModelPart head;
     private final ModelPart body;
-    private final ModelPart rightLeg;
-    private final ModelPart leftLeg;
-    private final ModelPart rightWing;
-    private final ModelPart leftWing;
-    private final ModelPart beak;
-    private final ModelPart beigeThing;
-    private final ModelPart mohawk;
-    private final ModelPart tail;
+    private final ModelPart head;
 
+    private final ModelPart beak;
+    private final ModelPart leg0;
+    private final ModelPart leg1;
+    private final ModelPart wing0;
+    private final ModelPart wing1;
 
     public ChockenModel(ModelPart root) {
-        this.head = root.getChild("head");
-        this.beak = root.getChild("beak");
-        this.beigeThing = root.getChild("beige_thing");
         this.body = root.getChild("body");
-        this.rightLeg = root.getChild("right_leg");
-        this.leftLeg = root.getChild("left_leg");
-        this.rightWing = root.getChild("right_wing");
-        this.leftWing = root.getChild("left_wing");
-        this.mohawk = root.getChild("mohawk");
-        this.tail = root.getChild("tail");
+        this.head = root.getChild("head");
+        this.beak = this.head.getChild("beak");
+        this.leg0 = root.getChild("leg0");
+        this.leg1 = root.getChild("leg1");
+        this.wing0 = root.getChild("wing0");
+        this.wing1 = root.getChild("wing1");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-        partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -6.0F, -2.0F, 4.0F, 6.0F, 3.0F), PartPose.offset(0.0F, 15.0F, -4.0F));
-        partdefinition.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(22, 23).addBox(-4.0F, -4.0F, 0.0F, 8.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 6.0F, 5.0F, -0.7854F, 0.0F, -3.1416F));
-        partdefinition.addOrReplaceChild("beak", CubeListBuilder.create().texOffs(14, 0).addBox(-2.0F, -4.0F, -4.0F, 4.0F, 2.0F, 2.0F), PartPose.offset(0.0F, 15.0F, -4.0F));
-        partdefinition.addOrReplaceChild("beige_thing", CubeListBuilder.create().texOffs(14, 4).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(0.0F, 15.0F, -4.0F));
-        partdefinition.addOrReplaceChild("mohawk", CubeListBuilder.create().texOffs(2, 25).addBox(-1.0F, -1.5F, 0.0F, 3.0F, 3.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -7.5F, -1.0F, 0.0F, -1.5708F, 0.0F));
-        partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 9).addBox(-3.0F, -4.0F, -3.0F, 6.0F, 8.0F, 6.0F), PartPose.offsetAndRotation(0.0F, 16.0F, 0.0F, ((float)Math.PI / 2F), 0.0F, 0.0F));
-        CubeListBuilder cubelistbuilder = CubeListBuilder.create().texOffs(26, 0).addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F);
-        partdefinition.addOrReplaceChild("right_leg", cubelistbuilder, PartPose.offset(-2.0F, 19.0F, 1.0F));
-        partdefinition.addOrReplaceChild("left_leg", cubelistbuilder, PartPose.offset(1.0F, 19.0F, 1.0F));
-        partdefinition.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(24, 13).addBox(0.0F, 0.0F, -3.0F, 1.0F, 4.0F, 6.0F), PartPose.offset(-4.0F, 13.0F, 0.0F));
-        partdefinition.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(24, 13).addBox(-1.0F, 0.0F, -3.0F, 1.0F, 4.0F, 6.0F), PartPose.offset(4.0F, 13.0F, 0.0F));
+
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 9).addBox(-3.0F, -4.0F, -3.0F, 6.0F, 8.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 16.0F, 0.0F, 1.5708F, 0.0F, 0.0F));
+
+        PartDefinition tail_r1 = body.addOrReplaceChild("tail_r1", CubeListBuilder.create().texOffs(22, 23).addBox(-4.0F, -4.0F, 0.0F, 8.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 6.0F, 5.0F, -0.7854F, 0.0F, -3.1416F));
+
+        PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -6.0F, -2.0F, 4.0F, 6.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 15.0F, -4.0F));
+
+        PartDefinition mohawk_r1 = head.addOrReplaceChild("mohawk_r1", CubeListBuilder.create().texOffs(2, 25).addBox(-1.0F, -1.5F, 0.0F, 3.0F, 3.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -7.5F, -1.0F, 0.0F, -1.5708F, 0.0F));
+
+        PartDefinition beak = head.addOrReplaceChild("beak", CubeListBuilder.create().texOffs(14, 0).addBox(-2.0F, -4.0F, -4.0F, 4.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition leg0 = partdefinition.addOrReplaceChild("leg0", CubeListBuilder.create().texOffs(26, 0).addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 19.0F, 1.0F));
+
+        PartDefinition leg1 = partdefinition.addOrReplaceChild("leg1", CubeListBuilder.create().texOffs(26, 0).addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(1.0F, 19.0F, 1.0F));
+
+        PartDefinition wing0 = partdefinition.addOrReplaceChild("wing0", CubeListBuilder.create().texOffs(24, 13).addBox(-1.0F, 0.0F, -3.0F, 1.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 13.0F, 0.0F));
+
+        PartDefinition wing1 = partdefinition.addOrReplaceChild("wing1", CubeListBuilder.create().texOffs(24, 13).addBox(0.0F, 0.0F, -3.0F, 1.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(3.0F, 13.0F, 0.0F));
+
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
 
     protected Iterable<ModelPart> headParts() {
-        return ImmutableList.of(this.head, this.beak, this.beigeThing, this.mohawk);
+        return ImmutableList.of(this.head);
     }
 
     protected Iterable<ModelPart> bodyParts() {
-        return ImmutableList.of(this.body, this.rightLeg, this.leftLeg, this.rightWing, this.leftWing, this.tail);
+        return ImmutableList.of(this.body, this.leg0, this.leg1, this.wing0, this.wing1);
     }
 
     public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.head.xRot = headPitch * ((float)Math.PI / 180F);
         this.head.yRot = netHeadYaw * ((float)Math.PI / 180F);
-        this.beak.xRot = this.head.xRot;
-        this.beak.yRot = this.head.yRot;
-        this.mohawk.xRot = this.head.xRot;
-        this.mohawk.yRot = this.head.yRot;
-        this.beigeThing.xRot = this.head.xRot;
-        this.beigeThing.yRot = this.head.yRot;
-        this.rightLeg.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        this.leftLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-        this.rightWing.zRot = ageInTicks;
-        this.leftWing.zRot = -ageInTicks;
+        this.leg0.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.leg1.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.wing0.zRot = ageInTicks;
+        this.wing1.zRot = -ageInTicks;
     }
 }
